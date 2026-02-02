3f38fec133107c4ef329a836ecf0a16b33ab0196
skar777123 <sanskargaikwad206@gmail.com>
2026-02-02 14:39:15 +0530

    drepto 27-5

--- src/shipping-address/shipping-address.module.ts
+++ src/shipping-address/shipping-address.module.ts
@@ -3,9 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ShippingAddressController } from './shipping-address.controller';
 import { ShippingAddressService } from './shipping-address.service';
 import { ShippingAddress, ShippingAddressSchema } from '../schemas/shipping-address.schema';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: ShippingAddress.name, schema: ShippingAddressSchema }])],
+    imports: [
+        MongooseModule.forFeature([{ name: ShippingAddress.name, schema: ShippingAddressSchema }]),
+        AuthModule
+    ],
     controllers: [ShippingAddressController],
     providers: [ShippingAddressService],
     exports: [ShippingAddressService],
